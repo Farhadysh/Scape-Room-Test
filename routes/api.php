e6cda37e7ce6ddd7271d47a73d57749535b2ef4f
Farhad Yozbashi <farhadysh@pop-os.localdomain>
2023-06-07 10:13:46 +0200

    Make Scape Room Tests

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\V1\ScapeRoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+
+    Route::name('scapeRooms.')->prefix('scapeRooms')->group(function () {
+        Route::get('/', [ScapeRoomController::class, 'index']);
+        Route::get('/{id}', [ScapeRoomController::class, 'show']);
+        Route::get('/{id}/timeSlots', [ScapeRoomController::class, 'timeSlots']);
+    });
+
+    
 });
---
+++ tests/Feature/ScapeRoomTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ScapeRoomTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_scape_room_index(): void
+    {
+        $response = $this->get('api/v1/scape_rooms');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_scape_room_show(): void
+    {
+        $response = $this->get('api/v1/scape_rooms/1');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_scape_room_timeSlots(): void
+    {
+        $response = $this->get('api/v1/scape_rooms/1/timeSlots');
+
+        $response->assertStatus(200);
+    }
+}
